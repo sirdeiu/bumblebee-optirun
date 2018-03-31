fde87b69078f5a2f6f6e885f7a6f48ecd5692a33
Andrei Amuraritei <sirdeiu@gmail.com>
2018-03-31 21:56:50 +0300

    removed spaces

--- runwithoptirun.sh
+++ runwithoptirun.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
 logfile='/tmp/runwithoptirun.log'
-###
 CPWD=$PWD
-
 APP=$1
 APP_PATH=$(dirname "${APP}")
 APP_FILE=$(basename "${APP}")
-
 if [[ "${APP_FILE}" =~ ".desktop" ]]; then
 	APP_EXEC=`grep Exec ${APP} | cut -d "=" -f2 | tr -d \"`
 	APP_PATH=`grep Path ${APP} | cut -d "=" -f2 | tr -d \"`
 #	echo "Got Path:${APP_PATH} Exec:${APP_EXEC} from ${APP_FILE}" >> $logfile
         APP_FILE=$(basename "${APP_EXEC}")
 fi
-
 cd "${APP_PATH}"
-
 #echo `pwd`
 optirun ./${APP_FILE} >> $logfile 2>&1 
-
 cd "${CPWD}"
